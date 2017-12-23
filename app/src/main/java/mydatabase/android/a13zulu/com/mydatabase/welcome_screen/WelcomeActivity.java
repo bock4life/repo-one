92f178645cce4eb796b8a4c573e6b004aeb0cb40
vitali <bock4life@gmail.com>
2017-12-23 21:12:46 +0200

    added mvp classes for welcome screen

---
+++ app/src/main/java/mydatabase/android/a13zulu/com/mydatabase/welcome_screen/WelcomeActivity.java
@@ -0,0 +1,8 @@
+package mydatabase.android.a13zulu.com.mydatabase.welcome_screen;
+
+/**
+ * Created by vital on 23/12/2017.
+ */
+
+public class WelcomeActivity {
+}
---
+++ app/src/main/java/mydatabase/android/a13zulu/com/mydatabase/welcome_screen/WelcomePresenter.java
@@ -0,0 +1,6 @@
+package mydatabase.android.a13zulu.com.mydatabase.welcome_screen;
+
+
+
+public class WelcomePresenter {
+}
